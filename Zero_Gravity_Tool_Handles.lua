8c6ba8b8c03703f7014ef1c2754bdd811948b87e
maxbd <47075676+maxbd07@users.noreply.github.com>
2020-04-28 04:36:49 -0400

    Update Zero_Gravity_Tool_Handles.lua

--- Zero_Gravity_Tool_Handles.lua
+++ Zero_Gravity_Tool_Handles.lua
@@ -4,21 +4,20 @@
 
 
 local options = {
-    CaptureAtMagnitude = 5,
-    EquipToolOnCapture = true,
-    ToolFloatBackSpeed = 500,
-    ToolFloatBackResponsiveness = 20,
+	CaptureAtMagnitude = 5,
+    	EquipToolOnCapture = true,
+    	ToolFloatBackSpeed = 500,
+    	ToolFloatBackResponsiveness = 20,
     
-    HotKeys = {
-        Release = Enum.KeyCode.J,
-        Capture = Enum.KeyCode.K
-    }
+    	HotKeys = {
+		Release = Enum.KeyCode.J,
+        	Capture = Enum.KeyCode.K
+	}
 }
 
 
 
 local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 
 
@@ -33,122 +32,123 @@ local vA = {}
 
 
 local function FolderToolParts(pA)
-    local tvA = Instance.new("Folder")
-    tvA.Parent = pA
+	local tvA = Instance.new("Folder")
+    	tvA.Parent = pA
 
-    for lvA, lvB in next, pA:GetChildren() do
-        if lvB:IsA("BasePart") then
-            lvB.Parent = tvA
-        end
-    end
-
-    return tvA
+    	for lvA, lvB in next, pA:GetChildren() do
+        	if lvB:IsA("BasePart") then
+            		lvB.Parent = tvA
+        	end
+    	end
+	
+	return tvA
 end
 
 local function FindFirstTool()
-    for lvA, lvB in next, CHARACTER:GetDescendants() do
-        if lvB:IsA("Tool") and lvB:FindFirstChildOfClass("Part") then
-            return lvB
-        end
-    end
+	for lvA, lvB in next, CHARACTER:GetDescendants() do
+        	if lvB:IsA("Tool") and lvB:FindFirstChildOfClass("Part") then
+            		return lvB
+        	end
+	end
+	
+	for lvA, lvB in next, BACKPACK:GetDescendants() do
+        	if lvB:IsA("Tool") and lvB:FindFirstChildOfClass("Part") then
+            		return lvB
+        	end
+    	end
 
-    for lvA, lvB in next, BACKPACK:GetDescendants() do
-        if lvB:IsA("Tool") and lvB:FindFirstChildOfClass("Part") then
-            return lvB
-        end
-    end
 
-
-    return nil
+    	return nil
 end
 
 local function Release(pA)
-    local tvA = FolderToolParts(pA)
+	local tvA = FolderToolParts(pA)
 
 
-    if #tvA:GetChildren() > 0 then
-        for lvA, lvB in next, tvA:GetDescendants() do
-            if lvB:IsA("BasePart") then
-                lvB.CanCollide = true
+    	if #tvA:GetChildren() > 0 then
+        	for lvA, lvB in next, tvA:GetDescendants() do
+            		if lvB:IsA("BasePart") then
+                		lvB.CanCollide = true
 
-                local tvB = Instance.new("BodyForce")
-                tvB.Force = Vector3.new(0, lvB:GetMass() * workspace.Gravity, 0)
-                tvB.Parent = lvB
-            end
-        end
+                		local tvB = Instance.new("BodyForce")
+                		tvB.Force = Vector3.new(0, lvB:GetMass() * workspace.Gravity, 0)
+                		tvB.Parent = lvB
+            		end
+        	end
 
-        tvA.Parent = workspace
-
-
-        table.insert(vA, {tvA, pA})
-    end
+        	tvA.Parent = workspace
+		
+        	table.insert(vA, {tvA, pA})
+	end
 end
 
 local function Capture(pA)
-    if pA and pA[1] and pA[2] then
-        local tvA = pA[1]:GetChildren()
+	if pA and pA[1] and pA[2] then
+        	local tvA = pA[1]:GetChildren()
 
 
-        for lvA, lvB in next, tvA do
-            lvB.CanCollide = false
-        end
+        	for lvA, lvB in next, tvA do
+			lvB.CanCollide = false
+        	end
 
 
-        local tvB = Instance.new("Attachment")
-        tvB.Parent = tvA[1]
+        	local tvB = Instance.new("Attachment")
+        	tvB.Parent = tvA[1]
 
 
-        local tvC = Instance.new("Attachment")
-        tvC.Position = Vector3.new(0, 0, -4)
-        tvC.Parent = HUMANOID_ROOT_PART
+        	local tvC = Instance.new("Attachment")
+        	tvC.Position = Vector3.new(0, 0, -4)
+        	tvC.Parent = HUMANOID_ROOT_PART
 
 
-        local tvD = Instance.new("AlignPosition")
-        tvD.RigidityEnabled = false
-        tvD.Responsiveness = options.ToolFloatBackResponsiveness
-        tvD.MaxVelocity = options.ToolFloatBackSpeed
-        tvD.MaxForce = options.ToolFloatBackSpeed
+        	local tvD = Instance.new("AlignPosition")
+        	tvD.RigidityEnabled = false
+        	tvD.Responsiveness = options.ToolFloatBackResponsiveness
+        	tvD.MaxVelocity = options.ToolFloatBackSpeed
+        	tvD.MaxForce = options.ToolFloatBackSpeed
 
-        tvD.Attachment0 = tvB
-        tvD.Attachment1 = tvC
-        tvD.Parent = tvA[1]
-
-
-        repeat wait() until (tvA[1].Position - HUMANOID_ROOT_PART.Position).Magnitude < options.CaptureAtMagnitude
+        	tvD.Attachment0 = tvB
+        	tvD.Attachment1 = tvC
+        	tvD.Parent = tvA[1]
 
 
-        for lvA, lvB in next, pA[1]:GetChildren() do
-            local tvE = lvB:FindFirstChildOfClass("BodyForce")
+        	repeat
+			wait()
+		until (tvA[1].Position - HUMANOID_ROOT_PART.Position).Magnitude < options.CaptureAtMagnitude
+
+
+        	for lvA, lvB in next, pA[1]:GetChildren() do
+            		local tvE = lvB:FindFirstChildOfClass("BodyForce")
             
-            if tvE then
-                tvE:Destroy()
-            end
+            		if tvE then
+                		tvE:Destroy()
+            		end
 
-            lvB.Parent = pA[2]
-        end
-
-
-        tvB:Destroy()
-        tvC:Destroy()
-        tvD:Destroy()
+            		lvB.Parent = pA[2]
+        	end
 
 
-        pA[1]:Destroy()
-        pA[2].Parent = options.EquipToolOnCapture and CHARACTER or BACKPACK
+        	tvB:Destroy()
+        	tvC:Destroy()
+        	tvD:Destroy()
 
 
-        return true
-    end
+        	pA[1]:Destroy()
+        	pA[2].Parent = options.EquipToolOnCapture and CHARACTER or BACKPACK
 
-    return false
+
+        	return true
+	end
+	
+	return false
 end
 
 
 
 UserInputService.InputBegan:Connect(function(pA, pB)
-    if pB == false and pA.UserInputType == Enum.UserInputType.Keyboard then
-        if pA.KeyCode == options.HotKeys.Release then
-            local tvA = FindFirstTool()
+	if pB == false and pA.UserInputType == Enum.UserInputType.Keyboard then
+        	if pA.KeyCode == options.HotKeys.Release then
+            		local tvA = FindFirstTool()
 
 			if tvA then
 				if tvA.Parent ~= CHARACTER then
@@ -158,12 +158,12 @@ UserInputService.InputBegan:Connect(function(pA, pB)
 
 				Release(tvA)
 			end
-        elseif pA.KeyCode == options.HotKeys.Capture then
-            if #vA > 0 and Capture(vA[#vA]) then
-                table.remove(vA, #vA)
-            end
-        end
-    end
+		elseif pA.KeyCode == options.HotKeys.Capture then
+            		if #vA > 0 and Capture(vA[#vA]) then
+                		table.remove(vA, #vA)
+            		end
+        	end
+	end
 end)
 
 
