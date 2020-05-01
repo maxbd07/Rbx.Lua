05c8e70e0a3e4356fd347e7fb7433bf7c252f8a4
maxbd <47075676+maxbd07@users.noreply.github.com>
2020-05-01 15:24:12 -0400

    Add files via upload

---
+++ Tool Handle Wall Glitch.lua
@@ -0,0 +1,41 @@
+-- Tool handle wall glitch exploit
+-- Author: maxbd (maxbd07, maxbd123, 0xEB)
+
+-- Additional Credit: AtomicPhysics (Inferosel) - Showed me this glitch with an autoclicker because I didn't know about it, I made it a script.
+
+
+
+local options = {
+    Tool = "BoomBox",
+    
+    HotKeys = {
+        Start = Enum.KeyCode.Q
+    }
+}
+
+
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+
+
+local PLAYER = Players.LocalPlayer
+local CHARACTER = PLAYER.Character
+local BACKPACK = PLAYER.Backpack
+
+
+local vA = BACKPACK:FindFirstChild(options.Tool) or CHARACTER:FindFirstChild(options.Tool)
+
+
+
+UserInputService.InputBegan:Connect(function(UserInputInformation, isTyping)
+    if isTyping == false and UserInputInformation.UserInputType == Enum.UserInputType.Keyboard then
+        if UserInputInformation.KeyCode == options.HotKeys.Start then
+            for lvA = 1, 100 do
+                RunService.RenderStepped:Wait()
+                vA.Parent = (lvA % 2 == 0 and BACKPACK or CHARACTER)
+            end
+        end
+    end
+end)
